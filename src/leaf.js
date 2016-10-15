5d5d1fdd0674a0dd17948fbd571036e51a31c556
Giulian Drimba <giuliandrimba@gmail.com>
2016-10-14 23:25:20 -0300

    v1

--- src/leaf.js
+++ src/leaf.js
@@ -1,12 +1,23 @@
-module.exports = function(){
+module.exports = function(ctx){
+
+	var R = 10 * (1024 / ctx.canvas.width);
+
 	return {
 		x:0,
 		y:0,
+		radius: R,
 		draw: function(ctx){
 			ctx.strokeStyle = "#000";
-			ctx.strokeWidth = 1
+			ctx.fillStyle = "#000";
+			if(this.radius < 3 * (ctx.canvas.width / 1024)) {
+				ctx.fillStyle = "#000";
+			}
+			ctx.lineWidth = 1 * (ctx.canvas.width / 1024)
 			ctx.beginPath();
-			ctx.arc(ctx.canvas.width / 2 + this.x,ctx.canvas.height / 2 + this.y,2,0,2*Math.PI);
+			ctx.arc(ctx.canvas.width / 2 + this.x,ctx.canvas.height / 2 + this.y,this.radius,0,2*Math.PI);
+			if(this.radius < 3 * (ctx.canvas.width / 1024)) {
+				ctx.fill();	
+			}
 			ctx.stroke();
 		}
 	}
--- src/sketch.js
+++ src/sketch.js
@@ -4,23 +4,35 @@ var ctx = undefined;
 var env = undefined;
 var c = 5;
 var n = 0;
+var r = 0
+var leafRadius = 10;
 
 exports.setup = function(_ctx, _env){
   ctx = _ctx;
   env = _env;
+  leafRadius = 10 * (ctx.canvas.width / 1024);
+  c = 9 * (ctx.canvas.width / 1024);
 
   background("#fff");
 }
 
 exports.draw = function() {
   var theta = n * (137.3 * Math.PI / 180);
-  var r = c * Math.sqrt(n);
+  r = c * Math.sqrt(n);
+  if(leafRadius < 1) {
+    env.done();
+    return;
+  }
 
-  var l = leaf()
+  leafRadius *= 0.999;
+
+  var l = leaf(ctx)
+  l.radius = leafRadius;
   l.x = r * Math.cos(theta)
   l.y = r * Math.sin(theta)
   l.draw(ctx)
   n++;
+  c -= 0.001 * (ctx.canvas.width / 1024);
 }
 
 function background(color){
