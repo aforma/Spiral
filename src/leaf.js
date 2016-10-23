25c7370bbdad51371f8a68c6ae5b7aa1611c73d8
Giulian Drimba <giuliandrimba@gmail.com>
2016-10-23 00:30:45 -0200

    new version

--- src/leaf.js
+++ src/leaf.js
@@ -10,22 +10,62 @@ module.exports = function(ctx){
 		angle:0,
 		draw: function(ctx){
 			// ctx.strokeStyle = "#000";
+			ctx.lineWidth = 1 * (ctx.canvas.width / 1024);
+			var percent = this.radius / (50 * (ctx.canvas.width / 1024))
+			this.color = 255 * percent;
+			if(this.radius > (13.5  * (ctx.canvas.width / 1024)) && this.radius < (15  * (ctx.canvas.width / 1024))) {
+				this.drawPetal();
+			} else {
+				this.drawCircle();
+			}
+		},
+		drawCircle:function(){
+			ctx.fillStyle = "rgba("+this.color+","+this.color+","+this.color+",1)";
+			ctx.beginPath();
+			ctx.arc(this.x,this.y,this.radius * 0.3,0,2*Math.PI);
+			ctx.stroke();
+		},
+		drawTriangle:function(){
 			ctx.save();
-			ctx.fillStyle = "#333";
+			ctx.fillStyle = "rgba("+this.color+","+this.color+","+this.color+",1)";
 			ctx.beginPath();
 			ctx.translate(this.x, this.y)
 			ctx.rotate(this.angle);
 			ctx.moveTo(0, 0);
-			ctx.lineTo(this.radius * 3, this.radius / 2);
-			ctx.lineTo(0, this.radius / 2);
+			ctx.lineTo(this.radius * 6, this.radius * 3);
+			ctx.lineTo(0, this.radius * 3);
 			ctx.fill();	
 			ctx.stroke();	
 			ctx.fillStyle = "rgba(255,255,255,1)";
 			ctx.beginPath();
-			ctx.moveTo(0, this.radius / 2);
-			ctx.lineTo(this.radius * 2, this.radius / 2);
-			ctx.lineTo(0, this.radius);
+			ctx.moveTo(0, this.radius * 3);
+			ctx.lineTo(this.radius * 6, this.radius * 3);
+			ctx.lineTo(0, this.radius * 6);
 			ctx.lineTo(0, 0);
+			ctx.fill();	
+			ctx.stroke();	
+			ctx.restore();
+		},
+		drawPetal:function(){
+			var size = this.radius / (50 * (ctx.canvas.width / 1024));
+			size *= 4;
+			var numLines = 5;
+			var pointB = 50 * (ctx.canvas.width / 1024);
+			var pointX = 26 * (ctx.canvas.width / 1024);
+			var pointX2 = 100 * (ctx.canvas.width / 1024);
+			ctx.save();
+			ctx.fillStyle = "rgba("+this.color+","+this.color+","+this.color+",1)";
+			ctx.beginPath();
+			ctx.translate(this.x, this.y)
+			ctx.rotate(this.angle);
+			ctx.moveTo(0, 0);
+			var total = numLines * size
+			for(var i = 0; i < total; i++) {
+				var a = (-pointB * size) * (i/ total);
+				var b = (pointB * size) * (i/ total);
+				ctx.quadraticCurveTo((pointX * size),a,(pointX2 * size),0);
+				ctx.quadraticCurveTo((pointX * size),b,0,0);
+			}
 			ctx.fill();	
 			ctx.stroke();	
 			ctx.restore();
--- src/sketch.js
+++ src/sketch.js
@@ -5,13 +5,13 @@ var env = undefined;
 var c = 5;
 var n = 0;
 var r = 0
-var leafRadius = 10;
+var leafRadius = 20;
 
 exports.setup = function(_ctx, _env){
   ctx = _ctx;
   env = _env;
-  leafRadius = 20 * (ctx.canvas.width / 1024);
-  c = 10 * (ctx.canvas.width / 1024);
+  leafRadius = 50 * (ctx.canvas.width / 1024);
+  c = 9 * (ctx.canvas.width / 1024);
 
   background("#fff");
 }
@@ -19,22 +19,28 @@ exports.setup = function(_ctx, _env){
 exports.draw = function() {
   var theta = n * (137.3 * Math.PI / 180);
   r = c * Math.sqrt(n);
-  if(leafRadius < 1) {
+  if(leafRadius < (1 * ctx.canvas.width / 1024) || c < -3) {
     env.done();
     return;
   }
+
+  var t = (0.999 * (ctx.canvas.width / 1024))
 
   leafRadius *= 0.999;
 
   var l = leaf(ctx)
   l.radius = leafRadius;
-  l.color = 1;
+  l.color = (1 * (ctx.canvas.width / 1024));
   l.angle = theta;
   l.x = (ctx.canvas.width / 2) + r * Math.cos(theta)
   l.y = (ctx.canvas.height / 2) + r * Math.sin(theta)
   l.draw(ctx)
   n++;
-  c -= 0.001 * (ctx.canvas.width / 1024);
+  if(leafRadius < (15  * (ctx.canvas.width / 1024))) {
+    c -= 0.003 * (ctx.canvas.width / 1024);
+  } else {
+    c -= 0.001 * (ctx.canvas.width / 1024);
+  }
 }
 
 function background(color){
